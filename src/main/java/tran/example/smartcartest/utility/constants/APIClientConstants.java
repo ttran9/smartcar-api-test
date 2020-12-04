1d01e01e2ac89adf393146a686d5f9ff564d415e
ttran9 <toddtran9@gmail.com>
2020-12-03 16:30:04 -0800

    bean to hold authentication client to make API calls.

--- src/main/java/tran/example/smartcartest/utility/constants/APIClientConstants.java
+++ src/main/java/tran/example/smartcartest/utility/constants/APIClientConstants.java
@@ -1,30 +1,48 @@
-package tran.example.smartcartest.utility;
+package tran.example.smartcartest.utility.constants;
 
 import com.smartcar.sdk.AuthClient;
+import org.json.simple.JSONObject;
+import org.springframework.stereotype.Component;
+import tran.example.smartcartest.configuration.MyFileReader;
 
 /*
  * Constants to be used in the application.
  */
-public class ClientConstants {
+@Component
+public class APIClientConstants {
 
-    private static final String CLIENT_ID = System.getenv("CLIENT_ID");
-//    private static final String CLIENT_ID = "48c0b800-bc06-4668-87ba-4e1a00ffe28f";
-    private static final String CLIENT_SECRET = System.getenv("CLIENT_SECRET");
-//    private static final String CLIENT_SECRET = "c08a0168-f333-426b-9bb6-890180a53674";
-    private static final String PORT = System.getenv("TEST_PORT");
-//    private static final String PORT = "8000";
-    private static final String REDIRECT_URI = "http://localhost:" + PORT + "/exchange";
     private static final boolean TEST_MODE = true;
     private static final String[] SCOPE = {"required:read_vehicle_info", "read_odometer", "required:read_tires", "read_vin"};
+//    private static final String[] SCOPE = {"required:read_vehicle_info", "required:read_tires", "read_vin"};
+//    private static final String[] SCOPE = {"required:read_vehicle_info", "required:read_tires", "read_vin"};
+//    private static final String[] SCOPE = {"required:read_vehicle_info", "required:read_tires", "read_odometer"};
+//    private static final String[] SCOPE = {"required:read_vehicle_info", "required:read_tires", "read_odometer"};
 
     /*
      * An instance of a custom client required to make HTTP requests to the Smartcar API.
      */
-    public static AuthClient client = new AuthClient(
-            CLIENT_ID,
-            CLIENT_SECRET,
-            REDIRECT_URI,
-            SCOPE,
-            TEST_MODE
-    );
+    public static AuthClient client;
+
+    public APIClientConstants() {
+        readConfigurationContent();
+    }
+
+    private void readConfigurationContent() {
+        MyFileReader fileReader = new MyFileReader();
+
+        JSONObject jsonObject = fileReader.getObjects("/etc/test_config.json");
+
+        String clientId = fileReader.getContentFromKey(jsonObject, "CLIENT_ID");
+        String clientSecret = fileReader.getContentFromKey(jsonObject, "CLIENT_SECRET");
+        String port = fileReader.getContentFromKey(jsonObject, "PORT");
+        String redirectURI  = "http://localhost:" + port + "/exchange";
+
+        client = new AuthClient(
+                clientId,
+                clientSecret,
+                redirectURI,
+                SCOPE,
+                TEST_MODE
+        );
+    }
 }
