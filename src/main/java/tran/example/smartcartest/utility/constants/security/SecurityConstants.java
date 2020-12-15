4baf025396864db6201df32e23c7584b5fbebd01
ttran9 <toddtran9@gmail.com>
2020-12-14 19:34:11 -0800

    Constant used to check if user is logged in.

--- src/main/java/tran/example/smartcartest/utility/constants/security/SecurityConstants.java
+++ src/main/java/tran/example/smartcartest/utility/constants/security/SecurityConstants.java
@@ -2,8 +2,8 @@ package tran.example.smartcartest.utility.constants.security;
 
 public class SecurityConstants {
 
-    public static final String ROLE_PREFIX = "ROLE";
     public static final String USER_AUTHORITY = "User";
+    public static final String ANONYMOUS_USER = "anonymousUser";
 
     public static final String USER_EXISTS_ERROR = "The user already exists!";
     public static final String ROLE_EXISTS_FOR_USER_ERROR = "The role already exists for this user!";
