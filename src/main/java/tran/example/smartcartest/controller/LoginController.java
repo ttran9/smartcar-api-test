07fa9bb37b7d07d040ed64b960b9b607ded96e77
ttran9 <toddtran9@gmail.com>
2020-12-15 01:58:31 -0800

    Renaming form object.

--- src/main/java/tran/example/smartcartest/controller/LoginController.java
+++ src/main/java/tran/example/smartcartest/controller/LoginController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import tran.example.smartcartest.model.form.ApplicationUserForm;
+import tran.example.smartcartest.model.form.LoginForm;
 import tran.example.smartcartest.utility.controller.ControllerHelper;
 
 import static tran.example.smartcartest.utility.constants.KeyConstants.TITLE_KEY;
@@ -19,7 +19,7 @@ import static tran.example.smartcartest.utility.constants.view.ViewConstants.LOG
 public class LoginController {
 
     @GetMapping(LOGIN_PATH)
-    public String login(@ModelAttribute ApplicationUserForm applicationUserForm, Model model) {
+    public String login(@ModelAttribute LoginForm loginForm, Model model) {
         ControllerHelper.addAttribute(model, TITLE_KEY, LOGIN_TITLE);
         return LOGIN_VIEW_NAME;
     }
