dd9d331df77f1565fb646b6cbc6898d1938a80c2
ttran9 <toddtran9@gmail.com>
2020-12-14 19:35:32 -0800

    Moving text to constants file.

---
+++ src/main/java/tran/example/smartcartest/utility/constants/view/TextConstants.java
@@ -0,0 +1,7 @@
+package tran.example.smartcartest.utility.constants.view;
+
+public class TextConstants {
+
+    public static final String VIEW_VEHICLES_TEXT = "View Vehicle Information";
+    public static final String CHANGE_VEHICLE_BRAND_MESSAGE = "change brand";
+}
