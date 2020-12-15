ee1d01f4028208c3185b63e99ecb3fe5868aeeef
ttran9 <toddtran9@gmail.com>
2020-12-14 19:33:30 -0800

    Information passed to the model object to build links which are shown to authenticated users with a valid access token.

--- src/main/java/tran/example/smartcartest/configuration/smartcar/constants/SmartCarApiConstants.java
+++ src/main/java/tran/example/smartcartest/configuration/smartcar/constants/SmartCarApiConstants.java
@@ -6,4 +6,7 @@ public class SmartCarApiConstants {
     public static final String CLIENT_SECRET_KEY = "CLIENT_SECRET";
     public static final String PORT_KEY = "PORT";
     public static final String DOMAIN_KEY = "DOMAIN";
+
+    public static final String CHANGE_VEHICLE_BRAND_KEY = "brandKey";
+    public static final String SMARTCAR_CONNECT_LINK = "https://connect.smartcar.com/oauth/flow";
 }
--- src/main/java/tran/example/smartcartest/utility/constants/KeyConstants.java
+++ src/main/java/tran/example/smartcartest/utility/constants/KeyConstants.java
@@ -4,4 +4,7 @@ public class KeyConstants {
 
     public static final String ERROR_KEY = "error";
     public static final String TITLE_KEY = "title";
+    public static final String VEHICLE_PATH_KEY = "vehiclePathKey";
+    public static final String SMARTCAR_CONNECT_LINK_KEY = "connectKey";
+    public static final String VEHICLE_TEXT_KEY = "vehicleTextKey";
 }
