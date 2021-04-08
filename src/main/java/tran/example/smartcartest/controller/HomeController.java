bc5ea1b2e8c65f58ba78f9762b74cd38969ab03a
ttran9 <toddtran9@gmail.com>
2021-04-08 01:34:47 -0700

    adding option for user to be able to go through auth flow if logged in but do not have an access token.

--- src/main/java/tran/example/smartcartest/controller/HomeController.java
+++ src/main/java/tran/example/smartcartest/controller/HomeController.java
@@ -1,22 +1,20 @@
 package tran.example.smartcartest.controller;
 
 import com.smartcar.sdk.SmartcarException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import tran.example.smartcartest.service.security.TokenValidationService;
 import tran.example.smartcartest.utility.controller.ControllerHelper;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
-import static org.springframework.security.web.context.HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY;
+import static tran.example.smartcartest.configuration.smartcar.constants.SmartCarApiConstants.CHANGE_VEHICLE_BRAND_KEY;
+import static tran.example.smartcartest.configuration.smartcar.constants.SmartCarApiConstants.SMARTCAR_CONNECT_LINK;
 import static tran.example.smartcartest.utility.constants.KeyConstants.*;
 import static tran.example.smartcartest.utility.constants.PathConstants.HOME_PATH;
 import static tran.example.smartcartest.utility.constants.PathConstants.VEHICLE_PATH;
 import static tran.example.smartcartest.utility.constants.ValueConstants.HOME_TITLE;
 import static tran.example.smartcartest.utility.constants.security.SecurityConstants.ANONYMOUS_USER;
+import static tran.example.smartcartest.utility.constants.view.TextConstants.GET_VEHICLE_INFORMATION_MESSAGE;
 import static tran.example.smartcartest.utility.constants.view.TextConstants.VIEW_VEHICLES_TEXT;
 import static tran.example.smartcartest.utility.constants.view.ViewConstants.HOME_VIEW_NAME;
 
@@ -34,13 +32,15 @@ public class HomeController {
 
     @GetMapping(HOME_PATH)
     public String displayHomePage(Model model) {
-
         String userName = ControllerHelper.getLoggedInUserName();
         try {
             if(!userName.equals(ANONYMOUS_USER)) {
                 if(tokenValidationService.checkToken(userName)) {
                     ControllerHelper.addAttribute(model, VEHICLE_PATH_KEY, VEHICLE_PATH);
                     ControllerHelper.addAttribute(model, VEHICLE_TEXT_KEY, VIEW_VEHICLES_TEXT);
+                } else {
+                    ControllerHelper.addAttribute(model, SMARTCAR_CONNECT_LINK_KEY, SMARTCAR_CONNECT_LINK);
+                    ControllerHelper.addAttribute(model, CHANGE_VEHICLE_BRAND_KEY, GET_VEHICLE_INFORMATION_MESSAGE);
                 }
             }
         } catch (SmartcarException exception) {
