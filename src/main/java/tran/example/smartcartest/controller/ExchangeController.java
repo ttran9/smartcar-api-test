7c7c9381dc02ec6c47bc18beb2c650f059d84070
ttran9 <toddtran9@gmail.com>
2021-04-08 01:33:52 -0700

    adding path to remove a user's access token.

--- src/main/java/tran/example/smartcartest/controller/ExchangeController.java
+++ src/main/java/tran/example/smartcartest/controller/ExchangeController.java
@@ -3,6 +3,7 @@ package tran.example.smartcartest.controller;
 import com.smartcar.sdk.SmartcarException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
@@ -11,7 +12,9 @@ import tran.example.smartcartest.service.exchange.ExchangeService;
 import tran.example.smartcartest.service.security.CustomUserDetailsService;
 import tran.example.smartcartest.utility.controller.ControllerHelper;
 
+import static tran.example.smartcartest.configuration.smartcar.constants.SmartCarApiConstants.SMARTCAR_CONNECT_LINK;
 import static tran.example.smartcartest.utility.constants.PathConstants.EXCHANGE_PATH;
+import static tran.example.smartcartest.utility.constants.PathConstants.REMOVE_TOKEN_PATH;
 import static tran.example.smartcartest.utility.constants.view.ViewConstants.VEHICLE_VIEW_NAME;
 
 /*
@@ -45,5 +48,13 @@ public class ExchangeController {
             System.out.println(exception.getMessage());
         }
        return redirectView;
+    }
+
+    @GetMapping(REMOVE_TOKEN_PATH)
+    public RedirectView removeToken() {
+        RedirectView redirectView = new RedirectView(SMARTCAR_CONNECT_LINK);
+        String userName = ControllerHelper.getLoggedInUserName();
+        exchangeService.deleteAccessToken(userName);
+        return redirectView;
     }
 }
